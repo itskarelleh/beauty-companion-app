930df125f432dc7951bc3c4208574339a76d7468
itskarelleh <karellehofler@gmail.com>
2024-11-28 20:08:28 -0500

    renamed functions

--- modus-backend/assembly/index.ts
+++ modus-backend/assembly/index.ts
@@ -1,27 +1,103 @@
 // import { getAnalysis } from "./analysis";
 import { models } from "@hypermode/modus-sdk-as"
-import { EmbeddingsModel } from "@hypermode/modus-sdk-as/models/experimental/embeddings"
+import { Generat}
+import {
+  OpenAIChatModel,
+  ResponseFormat,
+  SystemMessage,
+  UserMessage,
+} from "@hypermode/modus-sdk-as/models/openai/chat"
 import { JSON } from "json-as"
 
-interface Conversation {
-    role: string,
-    content: string,
-    images: string[]
-}
+interface Conversation { role: String, content: String, images: String[] }
 
-function getAnalysis(conversation: Conversation):void {
-    const model = models.getModel<EmbeddingsModel>("janus-flow")
-    const input = model.createInput([`${conversation.role}\n${conversation.content}\n${conversation.images.join("\n")}`])
+function doAnalysis(conversation: Conversation): string {
+    const model = models.getModel<Auto("janus-flow")
+    const input = model.createInput([
+        new UserMessage(conversation.content.toString()),
+    ])
     const output = model.invoke(input)
 
-    const prediction: string[] = output.predictions.map((p: f32[]) => p.map((d: f32) => d.toString()).join("\n"))
+    console.log(JSON.stringify(output))
+
+    const prediction: string = output.choices[0].message.content
     console.log(JSON.stringify(prediction))
 
-    // return prediction;
+    // return JSON.stringify(prediction[0]);
+    return "Hello, World!"
 }
 
 function sayHello(name: string | null = null): string {
   return `Hello, ${name || "World"}!`;
 }
 
-export { getAnalysis, sayHello };
+export { doAnalysis, sayHello };
+
+
+
+// import torch
+// from janus.janusflow.models import MultiModalityCausalLM, VLChatProcessor
+// from janus.utils.io import load_pil_images
+
+
+// const model_path = "deepseek-ai/JanusFlow-1.3B"
+
+// specify the path to the model
+// const vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
+// const tokenizer = vl_chat_processor.tokenizer
+
+// vl_gpt = MultiModalityCausalLM.from_pretrained(
+//     model_path, trust_remote_code=True
+// )
+// vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+
+// conversation = [
+//     {
+//         "role": "User",
+//         "content": "<image_placeholder>\nConvert the formula into latex code.",
+//         "images": ["images/equation.png"],
+//     },
+//     {"role": "Assistant", "content": ""},
+// ]
+
+// # load images and prepare for inputs
+// pil_images = load_pil_images(conversation)
+// prepare_inputs = vl_chat_processor(
+//     conversations=conversation, images=pil_images, force_batchify=True
+// ).to(vl_gpt.device)
+
+// # # run image encoder to get the image embeddings
+// inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+
+// # # run the model to get the response
+// outputs = vl_gpt.language_model.generate(
+//     inputs_embeds=inputs_embeds,
+//     attention_mask=prepare_inputs.attention_mask,
+//     pad_token_id=tokenizer.eos_token_id,
+//     bos_token_id=tokenizer.bos_token_id,
+//     eos_token_id=tokenizer.eos_token_id,
+//     max_new_tokens=512,
+//     do_sample=False,
+//     use_cache=True,
+// )
+
+// answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+// print(f"{prepare_inputs['sft_format'][0]}", answer)
+
+// example query
+// query {
+//     analysis(conversations: [{role: "User", 
+//                               content: "What is this an image of?", 
+//                               images: ["https://images.pexels.com/photos/19797390/pexels-photo-19797390/free-photo-of-beautiful-model-in-oregon-wearing-a-forest-green-linen-dress-portrait-taken-by-portland-photographer-lance-reis-on-my-sonya7iii-on-location.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"]
+//                              }
+//              ] 
+//       )
+//   }
+
+// query {
+//     analysis(conversation: {
+//         role: "User", 
+//         content: "What is this an image of?", 
+//         images: ["https://images.pexels.com/photos/19797390/pexels-photo-19797390/free-photo-of-beautiful-model-in-oregon-wearing-a-forest-green-linen-dress-portrait-taken-by-portland-photographer-lance-reis-on-my-sonya7iii-on-location.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"]
+//     })
+// }
